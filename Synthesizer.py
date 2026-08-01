3d781709a4bcf1f2489ac61553f508ccbcc62202
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-01 19:35:05 -0400

    Add import to synthesizer.

--- Synthesizer.py
+++ Synthesizer.py
@@ -31,6 +31,9 @@ class Synthesizer:
         importNode = ast.parse("from registered import *").body[0]
         self.pythonAst.body.insert(0, importNode)
 
+        importNode = ast.parse("from WorldState import worldState").body[0]
+        self.pythonAst.body.insert(0, importNode)
+
         synthSrc = ast.unparse(self.pythonAst)
 
         if self.stream:
