e8983e1bf82735349346d3d94fb4ee410cca8851
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-01 18:32:57 -0400

    Add skeleton for WorldState class.

---
+++ output_helpers/WorldState.py
@@ -0,0 +1,23 @@
+import uuid
+
+class WorldState:
+    '''
+        This class will contain the world state of the design. It will
+        assign a UID to each participant that is added and then it will
+        return the value when requested or the UID + value.
+    '''
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get(self):
+        pass
+
+    def update(self):
+        pass
