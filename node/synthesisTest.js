bdef5e774c30a40a9f2f074c11d259eb533f78e4
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-12 11:04:41 -0400

    Add direct synthesis from dal file in synthesizer.

--- node/synthesisTest.js
+++ node/synthesisTest.js
@@ -4,15 +4,26 @@ import fs from 'fs/promises';
 import { writeFile } from "node:fs/promises";
 import { json } from 'stream/consumers';
 import unzipper from "unzipper";
+import {DalAstGenerator} from "dal-ast-js";
+import { copyFile, rm, mkdir } from "fs/promises";
 
+const testStreamMode = async (pathToDesign, behavior) => {    
+    const data = await fs.readFile(pathToDesign);
 
-const testStreamMode = async (pathToAst, behavior) => {    
-    const data = await fs.readFile(pathToAst);
+    const ast = new DalAstGenerator().run(data.toString());
 
     try {
-        const synthesizedOutput = await synthesisRunner(data);
-        const output = JSON.parse(synthesizedOutput);
-        console.log("Synthesizer output:", output);
+        const synthesizedOutput = await synthesisRunner(JSON.stringify(ast));
+
+        const synthObj = JSON.parse(synthesizedOutput.toString());
+        const synthPath = path.join(process.cwd(), "node", "output");
+        await rm(synthPath, { recursive: true, force: true });
+        await mkdir(synthPath, { recursive: true });
+
+        for (const [name, value] of Object.entries(synthObj)) {
+            const filePath = path.join(process.cwd(), "node", "output", name);
+            await fs.writeFile(filePath, value);
+        }
     } catch (err) {
         console.error("Error during synthesis execution:");
         console.error(err);
